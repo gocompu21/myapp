0d5f7fb8023f60a4c6a34197e121f0d8aa7beb29
Apple <Apple@Apple-ui-MacBook-Pro.local>
2017-01-25 13:38:19 +0900

    데이터베이스 시딩

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -12,12 +12,23 @@
 */
 
 $factory->define(App\User::class, function (Faker\Generator $faker) {
-    static $password;
 
     return [
         'name' => $faker->name,
         'email' => $faker->unique()->safeEmail,
-        'password' => $password ?: $password = bcrypt('secret'),
+        'password' => bcrypt('password'),
         'remember_token' => str_random(10),
+    ];
+});
+
+$factory->define(App\Article::class, function (Faker\Generator $faker) {
+    $date = $faker->dateTimeThisMonth;
+    $userId = App\User::pluck('id')->toArray();
+
+    return [
+        'title' => $faker->sentence(),
+        'content' => $faker->paragraph(),
+        'created_at' => $date,
+        'updated_at' => $date,
     ];
 });
---
+++ public/index2.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Apple
+ * Date: 2017. 1. 25.
+ * Time: AM 2:10
+ */
+
+phpinfo();
+?>
+
